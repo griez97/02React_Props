2cb2ff70aade8bce7ff9453717508ffa48a9e662
griez97 <bryniarski.daniel@gmail.com>
2024-12-03 15:26:37 +0100

    jpg path change

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -4,7 +4,7 @@ import "./Home.css"
 const Home = () => {
     return ( 
         <>
-        <Item img="../../public/img/IMG1.jpg" title="Produkt 1" price="20$" btn="Buy Now!" />
+        <Item img="img/IMG1.jpg" title="Produkt 1" price="20$" btn="Buy Now!" />
 
         <Item img="img/IMG2.jpeg" title="Produkt 2" price="90$" btn="Buy Now!" />
 
